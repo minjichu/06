b573e96a16591d1c3279511a973ca574dfca6e23
minjichu <key0876@naver.com>
2018-11-08 15:49:01 +0900

    06_2

--- main.c
+++ main.c
@@ -4,14 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=365;
+	int x= 10;
+	int y= 20;
+	int*ptr;
+	ptr = &x;
+	printf("x : %i\n", *ptr);
 	
-	int*ptr =&i;
-
-	printf("i adress : %i\n", & i);
-	printf("i value : %i\n", i);
+	ptr =&y;
+	printf("y: %i\n", *ptr);
 	
-	printf("ptr adress : %i\n", ptr);
-	printf("ptr value : %i\n", *ptr);
+	ptr= &x;
+	*ptr = 20;
+	printf (" x: %i\n",x);
 	return 0;
 }
